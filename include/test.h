87dfc0c5ee93260e396996b19cb21e3a89fd763e
long_kejie <1721248012@qq.com>
2020-11-07 07:16:02 -0800

    first_version2 ans updata RAMDME.md

--- include/test.h
+++ include/test.h
@@ -8,9 +8,9 @@
 #ifndef _TEST_H
 #define _TEST_H
 
-#define TEST(a,b) void a##_##b()
+#define TEST(a,b) __attribute__((constructor)) void a##_##b()
 
-#define EXPECT_EQ(a,b) printf("%s == %s ? %s",#a,#b,a == b ? "TRUE" : "FALSE");
+#define EXPECT_EQ(a,b) printf("%s == %s ? %s\n",#a,#b,a == b ? "TRUE" : "FALSE");
 
 int RUN_ALL_TESTS() ;
 
